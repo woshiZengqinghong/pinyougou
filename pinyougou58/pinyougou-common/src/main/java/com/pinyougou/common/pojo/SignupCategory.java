d86610b64998288a0122626e687bbe7eecdd4a44
qinghong <qinghong@163.com>
2019-07-28 16:14:35 +0800

    我要提交

---
+++ pinyougou58/pinyougou-common/src/main/java/com/pinyougou/common/pojo/SignupCategory.java
@@ -0,0 +1,26 @@
+package com.pinyougou.common.pojo;
+
+public class SignupCategory {
+
+    private int typeId;
+
+    //你修改一下好不好
+    private String typeName;
+
+    public void setTypeId(int typeId) {
+
+        this.typeId = typeId;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+}
